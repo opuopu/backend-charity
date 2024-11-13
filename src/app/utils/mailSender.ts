e187881ef673423d797ddad7b988b17dfeeea5c5
opuopu <nurmdopu428@gmail.com>
2024-11-13 11:18:10 +0600

    fix

--- src/app/utils/mailSender.ts
+++ src/app/utils/mailSender.ts
@@ -3,18 +3,18 @@ import config from '../config';
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com.',
+    host: 'mail.privateemail.com',
     port: 587,
     secure: config.NODE_ENV === 'production',
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: config.nodemailer_host_email,
-      pass: config.nodemailer_host_pass,
+      user: 'info@helpforpakstudents.com',
+      pass: 'AAHFPS11#@#@',
     },
   });
 
   await transporter.sendMail({
-    from: 'nurmdopu428@gmail.com', // sender address
+    from: 'info@helpforpakstudents.com', // sender address
     to, // list of receivers
     subject,
     text: '', // plain text body
